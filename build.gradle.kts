5b3d0b3f2cf26b2f0dd21e9570e65c4fa1d38e86
Greg Anderson <greg.anderson.cs@gmail.com>
2020-11-17 20:33:51 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.0"
+    application
+    id("org.openjfx.javafxplugin") version "0.0.9"
+}
+group = "com.test"
+version = "1.0-SNAPSHOT"
+
+val tornadofx_version: String by rootProject
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClassName = "com.example.MainKt"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:$tornadofx_version")
+    testImplementation(kotlin("test-junit"))
+}
+
+javafx {
+    version = "13"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
